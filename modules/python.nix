07d04c12d6780365a669128184c1870d88c41e16
Adrian Danao-Schroeder <adriandanao@gmail.com>
2025-06-25 22:32:38 -0400

    more python packages

--- modules/python.nix
+++ modules/python.nix
@@ -1,7 +1,14 @@
-{ pkgs  ... }: {
+{ pkgs, ... }: {
   home.packages = with pkgs;
     [
-      (python3.withPackages
-        (python-pkgs: with python-pkgs; [ pandas requests ]))
+      (python3.withPackages (python-pkgs:
+        with python-pkgs; [
+          pandas
+          requests
+          numpy
+          matplotlib
+          datetime
+          pytest
+        ]))
     ];
 }
